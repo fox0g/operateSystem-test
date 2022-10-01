8925c1c2285d0d697ce8445e6164cf196a91b3d3
Fox0g <Fox0g@fox.com>
2022-10-01 15:19:35 +0800

    hot-fix commit-1

--- src/com/fox/os1/test.java
+++ src/com/fox/os1/test.java
@@ -62,7 +62,7 @@ public class test {
 
         System.out.println("平均周转时间:"+sum1/JCBArr.size());
         System.out.println("平均带权周转时间:"+sum2/JCBArr.size());
-
+        System.out.println("------------------");
 
     }
 }
